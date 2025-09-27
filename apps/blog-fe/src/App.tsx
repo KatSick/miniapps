b4602249e51ebaad99d5ced4f8cff5a99c9a5fb6
Ostap <chervakostap@gmail.com>
2025-09-27 10:21:41 +0300

    use vitest

--- apps/blog-fe/src/App.tsx
+++ apps/blog-fe/src/App.tsx
@@ -3,7 +3,7 @@ import viteLogo from "/vite.svg";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
-function App() {
+export const App: React.FC = () => {
   const [count, setCount] = useState(0);
 
   return (
@@ -28,6 +28,4 @@ function App() {
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
-}
-
-export default App;
+};
---
+++ apps/blog-fe/src/tests/HelloWorld.test.tsx
@@ -0,0 +1,9 @@
+import { expect, test, vi } from "vitest";
+import { render } from "vitest-browser-react";
+import { App } from "../App";
+
+test("renders name", async () => {
+  const { getByText } = render(<App />);
+  const element = await vi.waitFor(() => getByText("Vite + React"));
+  expect(element).toMatchSnapshot();
+});

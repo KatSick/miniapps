70d90997cd5dd1af8bac702c7ffbb9e770229d34
Ostap Chervak <chervakostap@gmail.com>
2025-09-29 18:30:29 +0300

    test ci (#2)
    Signed-off-by: Ostap Chervak <chervakostap@gmail.com>

--- apps/blog-fe/src/tests/HelloWorld.test.tsx
+++ apps/blog-fe/src/tests/HelloWorld.test.tsx
@@ -1,9 +1,9 @@
-import { expect, test, vi } from "vitest";
+import { expect, test } from "vitest";
 import { render } from "vitest-browser-react";
 import { App } from "@/App";
 
 test("renders name", async () => {
   const { getByText } = render(<App />);
-  const element = await vi.waitFor(() => getByText("Vite + React"));
-  expect(element).toMatchSnapshot();
+
+  await expect.element(getByText("Vite + React")).toBeInTheDocument();
 });

dd1065533bbf377473c27f3f9091022a50734655
Ostap <chervakostap@gmail.com>
2026-02-14 19:55:57 +0200

    refactor(telemetry): remove old single-platform source files

--- packages/telemetry/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { createWebSdkLayer } from "./tracing";
-export type { TelemetryConfig } from "./tracing";
-export { reportWebVitals } from "./browser/web-vitals";
-export { initTracing } from "./init";
--- packages/telemetry/src/init.ts
+++
@@ -1,9 +0,0 @@
-import type { TelemetryConfig } from "./tracing";
-
-import { reportWebVitals } from "./browser/web-vitals";
-import { createWebSdkLayer } from "./tracing";
-
-export const initTracing = (config: TelemetryConfig): void => {
-  const layer = createWebSdkLayer(config);
-  void reportWebVitals(layer);
-};
